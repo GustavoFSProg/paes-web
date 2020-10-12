b68745d5ef774710db2c49c5ee26f89133b0b3c2
Gustavo <gustavosohne38@gmail.com>
2020-10-12 14:41:19 -0300

    end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './styles/global.css'
 import './pages/Landing/landing.css'
 import logo from './assets/Logo.svg'
+import { FiArrowRight } from 'react-icons/fi'
 
 interface TitleProps {
   text: string
@@ -13,7 +14,7 @@ function App() {
       <div className="content-wrapper">
         <img src={logo} alt="Happy"></img>
         <main>
-          <h1>leve Felicidad e para o Mundo!</h1>
+          <h1>Leve Felicidade para o Mundo!</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
 
@@ -22,7 +23,7 @@ function App() {
           <strong>Rio Grande do Sul</strong>
         </div>
         <a href="#" className="enter-app">
-          <span>enter</span>
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
         </a>
       </div>
     </div>
