b647ce6bb72e0b68a2c1525b49165c663ca4fc92
Gustavo <gustavosohne38@gmail.com>
2021-03-11 10:15:06 -0300

    feito

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowRight } from 'react-icons/fi'
-import logo from '../../assets/Logo.svg'
+import paes1 from '../../assets/paes.jpeg'
 import '../../styles/global.css'
 import './landing.css'
 
@@ -9,17 +9,17 @@ export default function Landing() {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logo} alt="Happy" />
+        <img src={paes1} alt="paes" />
         <main>
-          <h1>Leve Felicidade para o Mundo!</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          <h1>Encomende seus pães caseiros!</h1>
+          <p>Experimente já!.</p>
         </main>
 
         <div className="location">
           <strong>Novo Hamburgo</strong>
           <strong>Rio Grande do Sul</strong>
         </div>
-        <Link to="/app" className="enter-app">
+        <Link to="/" className="enter-app">
           <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
         </Link>
       </div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:3001/',
-
-  baseURL: 'https://happy-nlw3-app.herokuapp.com/',
+  // baseURL: 'https://happy-nlw3-app.herokuapp.com/',
 })
 
 export default api
